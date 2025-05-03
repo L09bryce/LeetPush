5a8ba297ed2bda74d35bd0af3d598997cb2aa819
Bryson Gichuhi <156302026+L09bryce@users.noreply.github.com>
2025-05-03 18:53:15 +0300

    [1773] [Time Beats: 98.53%] [Memory Beats: 21.01%] - LeetPush

---
+++ LeetPush/Count-Items-Matching-a-Rule.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+         int i =0;
+         int key = 0;
+         switch (ruleKey) {
+            case "type": key =0; break;
+            case "color": key = 1; break;
+            case "name": key = 2; break;
+         }
+        
+
+        for(List<String> item : items){
+            if(item.get(key).equals(ruleValue)){
+                    i++;
+                }
+        }
+        return i;
+    }
+}
