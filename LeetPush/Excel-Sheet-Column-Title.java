760be1ae643440e829f739034b5d743d55612ed8
Bryson Gichuhi <156302026+L09bryce@users.noreply.github.com>
2025-05-06 10:04:26 +0300

    [168] [Time Beats: 100.00%] [Memory Beats: 69.71%] - LeetPush

---
+++ LeetPush/Excel-Sheet-Column-Title.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+        while(columnNumber > 0){
+            columnNumber--;
+            int rem = columnNumber % 26;
+            char ch = (char) ('A' + rem);
+            result.insert(0, ch);
+            columnNumber /= 26;
+        }
+        return result.toString();
+        
+    }
+}
