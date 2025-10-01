7e18d2514a0ed4211424a200d837b7b87fcbb96d
Bryson Gichuhi <156302026+L09bryce@users.noreply.github.com>
2025-10-01 08:40:44 +0300

    Time: 0 ms (100%), Space: 43 MB (51.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,7 +1,10 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target || nums[i] > target){
+            if(nums[i] == target){
+                return i;
+            }
+            if(nums[i] > target){
                 return i;
             }
         }
