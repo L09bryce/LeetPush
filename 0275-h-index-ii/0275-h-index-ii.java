2073ac9a5c1cfe28315a25505f2407d4244da691
Bryson Gichuhi <156302026+L09bryce@users.noreply.github.com>
2026-09-21 11:14:45 +0300

    Time: 0 ms (100%), Space: 50.2 MB (66.8%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int s = 0;
+        int e = citations.length -1;
+
+        while( s <= e){
+            int mid = s + (e -s) / 2;
+
+            if(citations[mid] == citations.length - mid){
+                return citations.length - mid;
+            }else if (citations[mid] < citations.length - mid) {
+			s = mid + 1;
+            }else { 
+            e = mid - 1;
+            }
+        }    
+        return citations.length - s;
+    }
+}
